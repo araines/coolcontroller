468918451a28a764a1632875c8df3a1de2f1486a
Andy Raines <andrew.raines@gmail.com>
2014-08-19 20:56:28 +0100

    Add first outline of control algorithm

--- coolcontroller.ino
+++ coolcontroller.ino
@@ -2,6 +2,9 @@
  * 2014 Andrew Raines
  */
 
+// Include PID control library - https://github.com/br3ttb/Arduino-PID-Library/
+#include <PID_v1.h>
+
 // DEBUG DEFINITIONS
 #define DEBUG
 #ifdef DEBUG
@@ -54,6 +57,12 @@
 // Program start
 // ====================
 
+// Set up PIDs
+double cold_temp, tec_pwm, cold_set_temp, hot_temp, hotfan_pwm, hot_set_temp;
+// Input (temp), Output (PWM), SetPoint (desired temp), Kp, Ki, Kd, direction
+PID cold_pid(&cold_temp, &tec_pwm, &cold_set_temp,2,5,1, DIRECT);
+PID hot_pid(&hot_temp, &hotfan_pwm, &hot_set_temp,2,5,1, DIRECT);
+
 void setup() {
   String msg;
   
@@ -73,11 +82,58 @@ void setup() {
 
 
 void loop() {
+
+}
+
+void doMenu() {
   if (encoderChanged()) {
     Serial.println(getEncoderPos());
     Serial.println(getEncoderBtn());
   }
-  if (getTemperatures()) {
-    
+}
+
+/* Main Control Algorithm
+ Basic Principle:
+ The TEC is controlled such that the duty cycle is increased to decrease the cool side,
+ or reduced if an alarm condition is met (MOSFET or hot side too hot).
+ The hot fan is controlled such that the duty cycle is increased to decrease the hot side
+ temperature to within operational temp, and reduced to reduce noise.
+ The cold fan is controlled manually by the user.
+*/
+void setupControl() {
+  // Initially set values to safe ones
+  cold_temp     = 0;
+  cold_set_temp = 999; // Switch off TEC
+  hot_temp      = 999;
+  hot_set_temp  = 0;   // Hot fan to 100%
+  
+  // Turn on PID controllers
+  cold_pid.SetOutputLimits(0,100);
+  hot_pid.SetOutputLimits(0,100);
+  cold_pid.SetMode(AUTOMATIC);
+  hot_pid.SetMode(AUTOMATIC);
+}
+
+#define HOT_ALARM_HI 80
+#define FET_ALARM_HI 80
+#define HOT_ALARM_LO 75
+#define FET_ALARM_LO 75
+
+void doControl() {
+  int hot_temp, cold_temp, fet_temp;
+  
+  // TODO: Read temperatures
+  
+  if (hot_temp > HOT_ALARM_HI || fet_temp > FET_ALARM_HI) {
+    // Alarm condition! Turn off the cold pid and set to safety
+    cold_pid.SetMode(MANUAL);
+    tec_pwm = 0;
+  } else if (hot_temp < HOT_ALARM_LO && fet_temp < FET_ALARM_LO) {
+    // Exit alarm condition.  Re-enable the cold pid
+    cold_pid.SetMode(AUTOMATIC);
   }
+  cold_pid.Compute();
+  hot_pid.Compute();
+  setHotFanSpeed(hotfan_pwm);
+  setColdFanSpeed(tec_pwm);
 }
